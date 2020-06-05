54be9332ee9fd122111ec65eb1a3aef3a4d1ffa7
Borota Igor <borota.igor@gmail.com>
2020-06-05 10:21:36 +0200

    Vejzbaju se rute!!!!

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -39,7 +39,12 @@ namespace EmployeeManagement
 
             app.UseStaticFiles();
 
-            app.UseMvcWithDefaultRoute();
+            //app.UseMvcWithDefaultRoute();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.Run(async (context) =>
             {

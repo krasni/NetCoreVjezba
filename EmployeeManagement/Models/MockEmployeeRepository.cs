00e7ee2d510255bd1ee5ce4067a7b59f5cb71721
Borota Igor <borota.igor@gmail.com>
2020-06-08 12:09:21 +0200

    Radi se

--- EmployeeManagement/Models/MockEmployeeRepository.cs
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -13,9 +13,9 @@ namespace EmployeeManagement.Models
         {
             _employeeList = new List<Employee>()
             {
-                new Employee(){ Id = 1, Name = "Merry", Department = "HR", Email = "myrry@gmail.com" },
-                new Employee(){ Id = 2, Name = "Igor", Department = "IT", Email = "igor@gmail.com" },
-                new Employee(){ Id = 3, Name = "Sasa", Department = "IT", Email = "sasa@gmail.com" }
+                new Employee(){ Id = 1, Name = "Merry", Department = Dept.HR, Email = "myrry@gmail.com" },
+                new Employee(){ Id = 2, Name = "Igor", Department = Dept.IT, Email = "igor@gmail.com" },
+                new Employee(){ Id = 3, Name = "Sasa", Department = Dept.IT, Email = "sasa@gmail.com" }
             };
         }
 

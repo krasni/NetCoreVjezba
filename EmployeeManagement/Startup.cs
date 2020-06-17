a10410064b1e2a20ff5c9fff00846ac487f2d721
Borota Igor <borota.igor@gmail.com>
2020-06-17 09:25:39 +0200

    Dodao SQL Repo

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -30,7 +30,7 @@ namespace EmployeeManagement
                 options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
 
             services.AddMvc();
-            services.AddTransient<IEmployeeRepository, MockEmployeeRepository>();
+            services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

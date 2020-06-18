b6dc45686686be7e854228f52d053d16b184b536
Borota Igor <borota.igor@gmail.com>
2020-06-18 12:36:40 +0200

    Radi se image upload

--- EmployeeManagement/ViewModels/EmployeeCreateViewModel.cs
+++ EmployeeManagement/ViewModels/EmployeeCreateViewModel.cs
@@ -1,26 +1,26 @@
-﻿using System;
+﻿using EmployeeManagement.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
-namespace EmployeeManagement.Models
+namespace EmployeeManagement.ViewModels
 {
-    public class Employee
+    public class EmployeeCreateViewModel
     {
-        public int Id { get; set; }
-
         [Required]
         [MaxLength(50, ErrorMessage = "Nemože duže od 50 karaktera")]
         public string Name { get; set; }
 
         [Required]
-        [Display(Name="Office Email")]
+        [Display(Name = "Office Email")]
         public string Email { get; set; }
 
         [Required]
         public Dept? Department { get; set; }
 
-        public string PhotoPath { get; set; }
+        public IFormFile Photo { get; set; }
     }
 }

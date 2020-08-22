def089cacd10950ac08e85fecf440ccdf0ce99d3
Borota Igor <borota.igor@gmail.com>
2020-08-22 12:09:05 +0200

    Radi  se ...

--- EmployeeManagement/Security/CanEditOnlyAdminRolesAndClaimsHandler.cs
+++ EmployeeManagement/Security/CanEditOnlyAdminRolesAndClaimsHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System;
 using System.Collections.Generic;
@@ -12,19 +13,27 @@ namespace EmployeeManagement.Security
     public class CanEditOnlyOtherAdminRolesAndClaimsHandler :
         AuthorizationHandler<ManageAdminRolesAndClaimsRequirement>
     {
+
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public CanEditOnlyOtherAdminRolesAndClaimsHandler(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+        }
+
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
             ManageAdminRolesAndClaimsRequirement requirement)
         {
-            var authFilterContext = context.Resource as AuthorizationFilterContext;
-            if (authFilterContext == null)
+            if (context.User == null || !context.User.Identity.IsAuthenticated)
             {
+                context.Fail();
                 return Task.CompletedTask;
             }
 
             string loggedInAdminId =
                 context.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
 
-            string adminIdBeingEdited = authFilterContext.HttpContext.Request.Query["userId"];
+            string adminIdBeingEdited = httpContextAccessor.HttpContext.Request.Query["userId"].ToString();
 
             if (context.User.IsInRole("Admin") &&
                 context.User.HasClaim(claim => claim.Type == "Edit Role" && claim.Value == "true") &&
